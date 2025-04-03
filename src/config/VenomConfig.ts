d1217c0cc20e394a4fe5da2cf31205f6c865b733
Erik Andrade <erikandrade621@gmail.com>
2025-04-02 22:00:41 -0300

    "feat: alterar função para abrir sessão"

--- src/config/VenomConfig.ts
+++ src/config/VenomConfig.ts
@@ -1,8 +1,13 @@
-import venom from 'venom-bot';
+import * as venom from 'venom-bot';
 
 export const startVenomClient = () => {
-  return venom.create({
-    session: 'session-name',
-    multidevice: true, // Habilita multiplos dispositivos
-  });
+  return venom.create(
+    'session-name', // Nome da sessão como primeiro argumento
+    undefined, // Parâmetro opcional (QR Code)
+    undefined, // Parâmetro opcional (statusFind)
+    {
+      headless: "new", // Executa sem abrir uma janela do navegador
+      autoClose: 0, // Mantém o WhatsApp aberto mesmo após erro    
+    }
+    );
 }; 
